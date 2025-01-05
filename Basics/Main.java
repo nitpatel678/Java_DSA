3f44b59092d04b316daf6396a6cee4b5aa8428a9
Nitin Patel <nitpatel678@gmail.com>
2025-01-06 01:05:31 +0530

    updating again

--- Basics/Main.java
+++ Basics/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        
+        Scanner sc = new Scanner(System.in);
+
+        // Close the scanner to free resources
+        sc.close();
     }
 }
