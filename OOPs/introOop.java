8ff2127fc1e2aeb93f33749838f49cb2ffab89b9
Nitin Patel <nitpatel678@gmail.com>
2025-01-08 00:25:38 +0530

    update

---
+++ OOPs/introOop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+// Classes are like the blueprint or the framework for the various objects
+// that we are building and the objects the material which are build through classes
+public class introOop {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+    }    
+}
+
