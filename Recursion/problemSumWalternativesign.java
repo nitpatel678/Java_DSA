c6b9b35c89d26d541c9783ed5eb6187e584383fc
Nitin Patel <nitpatel678@gmail.com>
2025-01-16 22:08:17 +0530

    update

--- Recursion/problemSumWalternativesign.java
+++ Recursion/problemSumWalternativesign.java
@@ -1,13 +1,30 @@
 import java.util.Scanner;
 
-class recursion{
-    
+class recursion {
+    static int print_sum(int n) {
+        // Base case
+        if (n == 0) {
+            return 0; // Return 0 when n is 0
+        } else if (n == 1) {
+            return 1; // Base case for n == 1
+        } else {
+            // Recursive case: Add or subtract based on even/odd condition
+            if (n % 2 == 0) {
+                return n + print_sum(n - 1); // Add if n is even
+            } else {
+                return -n + print_sum(n - 1); // Subtract if n is odd
+            }
+        }
+    }    
 }
 
 public class problemSumWalternativesign {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the no:");
+        int n = sc.nextInt();
+        int result = recursion.print_sum(n);
+        System.out.println("Result: " + result);
         // Close the scanner to free resources
         sc.close();
     }
