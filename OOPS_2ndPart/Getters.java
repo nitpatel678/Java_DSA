39030554272ac93f66cf6d52d41e55b54ada4999
Nitin Patel <nitpatel678@gmail.com>
2025-02-09 03:09:59 +0530

    Update

---
+++ OOPS_2ndPart/Getters.java
@@ -0,0 +1,16 @@
+public class Getters {
+        String name;
+        private int age;
+        double percent;
+    
+    // Getters
+    public int getRno(){
+        return rno;
+    }
+    
+    // Setters 
+    public void setRno(){
+        
+    }
+
+}
