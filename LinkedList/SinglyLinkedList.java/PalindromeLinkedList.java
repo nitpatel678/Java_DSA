f78b4599f1560f5d4b2095e60e2b133ebb82c757
Nitin Patel <nitpatel678@gmail.com>
2025-02-12 03:04:06 +0530

    Update

--- LinkedList/SinglyLinkedList.java/PalindromeLinkedList.java
+++ LinkedList/SinglyLinkedList.java/PalindromeLinkedList.java
@@ -1,5 +1,37 @@
 import java.util.*;
 public class PalindromeLinkedList {
+  
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static boolean IsPalindrome(Node head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        // Step 1: Store values in an array
+        Node temp = head;
+        while (temp != null) {
+            list.add(temp.data);
+            temp = temp.next;
+        }
+    
+        // Step 2: Check if the array is a palindrome
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            if (!list.get(left).equals(list.get(right))) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+    
+        return true; // Palindrome
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
