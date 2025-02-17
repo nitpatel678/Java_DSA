09a79c6c9ddef92b38a09838294935f4a8d096b3
Nitin Patel <nitpatel678@gmail.com>
2025-02-18 02:53:06 +0530

    Update

--- Queue/info.java
+++ Queue/info.java
@@ -1,9 +1,27 @@
 import java.util.*;
+
 public class info {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Initializing a Queue using LinkedList
+        Queue<Integer> q = new LinkedList<>();
+        
+        // Adding elements to the queue
+        q.add(5);
+        q.add(4);
+        q.add(3);
+        q.add(2);
+        q.add(1);
 
-        // Close the scanner to free resources
-        sc.close();
+        // Printing the queue
+        System.out.println("Queue before removal: " + q);
+
+        // Removing the front element (FIFO)
+        q.remove();
+
+        // Printing the queue after removal
+        System.out.println("Queue after removal: " + q);
+
+        // Print out the first element
+        System.out.println(q.element());
     }
 }
