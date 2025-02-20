4b0628c8c6b5b076568535b6e9971bb7e87ef67b
Nitin Patel <nitpatel678@gmail.com>
2025-02-20 23:07:00 +0530

    Update

--- HashMap/info.java
+++ HashMap/info.java
@@ -12,7 +12,37 @@ public class info {
         // getting value from a key
         System.out.println(mp.get("Nitin"));
         // Changing value of a key
-        
+        mp.put("Akash", 35);
+        /*
+         * It has unique value means that it will not create a new akash
+         * it will simply just update the akash value
+         */
+
+        // To remove the pair of key and value pair
+        mp.remove("Harshit");
+        mp.remove("Riya"); // It will return null
+
+        // Check if the key contain or not
+        System.out.println(mp.containsKey("Nitin"));
+
+        // Adding a new entry if it doesn't exist
+        mp.putIfAbsent("Yash" , 53);
+        mp.putIfAbsent("Ayllsa" , 53);
+
+        // Get all keys in the Hashmap
+        System.out.println(mp.keySet());
+
+        // Get all values in the Hashmap
+        System.out.println(mp.values());
+
+        // Get all the entries in the Hashmap
+        System.out.println(mp.entrySet());
+
+
+        // TRAVERSING ALL ENTERIES OF HASHMAP - Multiple Methods
+        for (String key : mp.keySet()) {
+            System.out.println("Age of " + key + " is " + mp.get(key));
+        }        
     }
 
     public static void main(String[] args) {
